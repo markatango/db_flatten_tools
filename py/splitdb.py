d3436ff2c28525f7d7d5b956ba5e6ffd8959a9e5
Mark Rodighiero <markatango@gmail.com>
2019-04-20 12:05:27 -0700

    anonymized the database params

--- py/splitdb.py
+++ py/splitdb.py
@@ -1,22 +1,25 @@
-# Database splitter and flattener.
-# splits based on 'tags.equipment_model'
-# flattens arrays and objects
+# MongoDB collection splitter and flattener.
+# Splits collection based on a field
+# Flattens documents in the collection
 
 # usage: In CLI: 'python dbflatten.py'
 
 # Specify database and collection to be flattened
+# and modify the obvious other things like host, username, etc
+# or modify program to accept them as arguments to main
 
 from pymongo import MongoClient
 from dbflatten import dbFlatten
 
-DATABASE = "amya"
-COLLECTION = "simulated"
+DATABASE = "dbname"
+COLLECTION = "collectionname"
+FIELD = "fieldname"
 
 # Globals
-client = MongoClient('amya.io:8002', \
-                        username='webuser', 
-                        password='webuser', \
-                        authsource='amya', \
+client = MongoClient('hostaddress:port', \
+                        username='username', 
+                        password='password', \
+                        authsource='authdb', \
                         authMechanism='SCRAM-SHA-1' \
                         )
 
@@ -24,11 +27,11 @@ col = client[DATABASE][COLLECTION]
 
 # Batches inserts in size 'batchSize' to conserve memory. 
 # Probably there's an optimal batchSize number.
-def createCollection(model, batchSize = 20):
+def createCollection(value, batchSize = 2000):
   global newdoc, col, client
   
-  docs = col.find({ "tags.equipment_model" : model  }).limit(50)
-  head_key = model
+  docs = col.find({ FIELD: value  })
+  head_key = value
   counter = 0
   docContainer = []
   
@@ -48,7 +51,7 @@ def createCollection(model, batchSize = 20):
   
     # # Insert the batch in a new collection defined by model
     # I don't know... insert or insertMany?
-    client[DATABASE][model].insert(docContainer)
+    client[DATABASE][value].insert(docContainer)
     docContainer = []
     
 # ==========================
@@ -56,17 +59,17 @@ def createCollection(model, batchSize = 20):
 #  Main loop
 #
 if __name__ == "__main__":
-  client = MongoClient('amya.io:8002', \
-                        username='webuser', 
-                        password='webuser', \
-                        authsource='amya', \
+  client = MongoClient('hostaddress:port', \
+                        username='username', 
+                        password='password', \
+                        authsource='authdb', \
                         authMechanism='SCRAM-SHA-1' \
                         )
   
   #get models from the amya.simulated collection
-  models = col.distinct("tags.equipment_model")
-  for  m in models:
-    print(m)
-    createCollection(m);
+  values = col.distinct("tags.equipment_model")
+  for  v in values:
+    print(v)
+    createCollection(v);
     # examine a sample flat document
-    print(str(client[DATABASE][m].find_one()))
+    print(str(client[DATABASE][v].find_one()))
